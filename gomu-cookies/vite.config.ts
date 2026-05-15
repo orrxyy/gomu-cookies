f8784c5f4571d9f7253d2a78db5206bb20b552ce
orrxyy <108250976+orrxyy@users.noreply.github.com>
2026-05-15 20:25:27 +0700

    Delete gomu-cookies directory

--- gomu-cookies/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  // Base harus sesuai dengan nama repository GitHub kamu
-  base: '/gomu-cookies/', 
-  plugins: [react(), tailwindcss()],
-})
