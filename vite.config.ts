d1fe8b6cbea7fd586b57a9ff0792ce1f9672d498
orrxyy <108250976+orrxyy@users.noreply.github.com>
2026-05-15 21:39:12 +0700

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  base: './', // Gunakan titik satu agar dia mencari di folder yang sama
-  plugins: [react(), tailwindcss()],
-})
